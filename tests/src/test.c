4b0eb9a455f55d6a666d9cb2b01f0bac14d2954b
Hatim-Pierre FAZILEABASSE <hatim-pierre.fazileabasse@ingenico.com>
2021-09-24 15:51:03 +0200

    [MEMPROT][TESTS] Restored most of the unit-tests
    Signed-off-by: Hatim-Pierre FAZILEABASSE <hatim-pierre.fazileabasse@ingenico.com>

--- tests/src/test.c
+++ tests/src/test.c
@@ -49,7 +49,7 @@ static int test_suit( struct test const* tests, int numtests ) {
     printf( "%s", "\n\nTests:\n" );
     int failed = 0;
     for( int i = 0; i < numtests; ++i ) {
-        printf( " %02d%s%-25s ", i, ": ", tests[i].name );
+        printf( " %02d%s%-25s \n", i, ": ", tests[i].name );
         int linerr = tests[i].func();
         if ( 0 == linerr )
             printf( "%s", "OK\n" );
@@ -65,64 +65,152 @@ static int test_suit( struct test const* tests, int numtests ) {
 
 
 // ----------------------------------------------------------- Unit tests: ---
+typedef struct json_errcodes_map {
+    json_errcodes_t errCode;
+    char funcName[64];
+} json_errcodes_map_t;
+
+static inline void printErrCodeMap(const json_errcodes_map_t map[],
+                                   const size_t mapSz)
+{
+    for (size_t i = 0; i < mapSz; i++)
+    {
+        printf("%s returned with 0x%02X\n", map[i].funcName, map[i].errCode);
+    }
+}
 
 static int escape( void ) {
     char buff[512];
-    char* p = json_objOpen( buff, NULL );
-    p = json_str( p, "name", "\tHello: \"man\"\n" );
-    p = json_objClose( p );
-    p = json_end( p );
+    json_buffer_t jsonBuffer = {
+            .buffer = buff,
+            .total_sz = sizeof(buff),
+            .remaining_sz = sizeof(buff)
+    };
+    json_errcodes_map_t errcodesMap[] = {
+            {.funcName = "json_start"},
+            {.funcName = "json_objOpen"},
+            {.funcName = "json_str"},
+            {.funcName = "json_objClose"},
+            {.funcName = "json_end"}
+    };
+
+    errcodesMap[0].errCode = json_start(&jsonBuffer);
+    errcodesMap[1].errCode = json_objOpen( &jsonBuffer, NULL );
+    errcodesMap[2].errCode = json_str( &jsonBuffer, "name", "\tHello: \"man\"\n" );
+    errcodesMap[3].errCode = json_objClose( &jsonBuffer );
+    errcodesMap[4].errCode = json_end( &jsonBuffer );
+    printErrCodeMap(errcodesMap, (sizeof(errcodesMap)/sizeof(errcodesMap[0])));
     printf( "\n\n%s\n\n", buff );
+
     static char const rslt[] = "{\"name\":\"\\tHello: \\\"man\\\"\\n\"}";
-    check( p - buff == sizeof rslt - 1 );
+    check(strlen(buff) == sizeof rslt - 1 );
     check( 0 == strcmp( buff, rslt ) );
     done();
 }
 
 static int len( void ) {
     char buff[512];
-    char* p = json_objOpen( buff, NULL );
-    p = json_nstr( p, "name", "\tHello: \"man\"\n", 6 );
-    p = json_objClose( p );
-    p = json_end( p );
+    json_buffer_t jsonBuffer = {
+            .buffer = buff,
+            .total_sz = sizeof(buff),
+            .remaining_sz = sizeof(buff)
+    };
+    json_errcodes_map_t errcodesMap[] = {
+            {.funcName = "json_start"},
+            {.funcName = "json_objOpen"},
+            {.funcName = "json_nstr"},
+            {.funcName = "json_objClose"},
+            {.funcName = "json_end"}
+    };
+    errcodesMap[0].errCode = json_start(&jsonBuffer);
+    errcodesMap[1].errCode = json_objOpen( &jsonBuffer, NULL );
+    errcodesMap[2].errCode = json_nstr( &jsonBuffer, "name", "\tHello: \"man\"\n", 6 );
+    errcodesMap[3].errCode = json_objClose( &jsonBuffer );
+    errcodesMap[4].errCode = json_end( &jsonBuffer );
+    printErrCodeMap(errcodesMap, sizeof(errcodesMap)/sizeof(errcodesMap[0]));
+    printf( "\n\n%s\n\n", buff );
+
     static char const rslt[] = "{\"name\":\"\\tHello\"}";
-    check( p - buff == sizeof rslt - 1 );
+    check(strlen(buff) == sizeof rslt - 1 );
     check( 0 == strcmp( buff, rslt ) );
     done();
 }
 
 static int empty( void ) {
     char buff[512];
+    json_buffer_t jsonBuffer = {
+            .buffer = buff,
+            .total_sz = sizeof(buff),
+            .remaining_sz = sizeof(buff)
+    };
     {
-        char* p = json_objOpen( buff, NULL );
-        p = json_objClose( p );
-        p = json_end( p );
+        json_errcodes_map_t errcodesMap[] = {
+                {.funcName = "json_start"},
+                {.funcName = "json_objOpen"},
+                {.funcName = "json_objClose"},
+                {.funcName = "json_end"}
+        };
+        errcodesMap[0].errCode = json_start(&jsonBuffer);
+        errcodesMap[1].errCode = json_objOpen( &jsonBuffer, NULL );
+        errcodesMap[2].errCode = json_objClose( &jsonBuffer );
+        errcodesMap[3].errCode = json_end( &jsonBuffer );
+        printErrCodeMap(errcodesMap, sizeof(errcodesMap)/sizeof(errcodesMap[0]));
+        printf( "\n\n%s\n\n", buff );
+
         static char const rslt[] = "{}";
-        check( p - buff == sizeof rslt - 1 );
+        check(strlen(buff) == sizeof rslt - 1 );
         check( 0 == strcmp( buff, rslt ) );
     }
     {
-        char* p = json_objOpen( buff, NULL );
-        p = json_arrOpen( p, "a" );
-        p = json_arrClose( p );
-        p = json_objClose( p );
-        p = json_end( p );
+        json_errcodes_map_t errcodesMap[] = {
+                {.funcName = "json_start"},
+                {.funcName = "json_objOpen"},
+                {.funcName = "json_arrOpen"},
+                {.funcName = "json_arrClose"},
+                {.funcName = "json_objClose"},
+                {.funcName = "json_end"}
+        };
+        errcodesMap[0].errCode = json_start(&jsonBuffer);
+        errcodesMap[1].errCode = json_objOpen( &jsonBuffer, NULL );
+        errcodesMap[2].errCode = json_arrOpen( &jsonBuffer, "a" );
+        errcodesMap[3].errCode = json_arrClose( &jsonBuffer );
+        errcodesMap[4].errCode = json_objClose( &jsonBuffer );
+        errcodesMap[5].errCode = json_end( &jsonBuffer );
+        printErrCodeMap(errcodesMap, sizeof(errcodesMap)/sizeof(errcodesMap[0]));
+        printf( "\n\n%s\n\n", buff );
+
         static char const rslt[] = "{\"a\":[]}";
-        check( p - buff == sizeof rslt - 1 );
+        check(strlen(buff) == sizeof rslt - 1 );
         check( 0 == strcmp( buff, rslt ) );
     }
     {
-        char* p = json_objOpen( buff, NULL );
-        p = json_arrOpen( p, "a" );
-        p = json_objOpen( p, NULL );
-        p = json_objClose( p );
-        p = json_objOpen( p, NULL );
-        p = json_objClose( p );
-        p = json_arrClose( p );
-        p = json_objClose( p );
-        p = json_end( p );
+        json_errcodes_map_t errcodesMap[] = {
+                {.funcName = "json_start"},
+                {.funcName = "json_objOpen"},
+                {.funcName = "json_arrOpen"},
+                {.funcName = "json_objOpen"},
+                {.funcName = "json_objClose"},
+                {.funcName = "json_objOpen"},
+                {.funcName = "json_objClose"},
+                {.funcName = "json_arrClose"},
+                {.funcName = "json_objClose"},
+                {.funcName = "json_end"}
+        };
+        errcodesMap[0].errCode = json_start(&jsonBuffer);
+        errcodesMap[1].errCode = json_objOpen( &jsonBuffer, NULL );
+        errcodesMap[2].errCode = json_arrOpen( &jsonBuffer, "a" );
+        errcodesMap[3].errCode = json_objOpen( &jsonBuffer, NULL );
+        errcodesMap[4].errCode = json_objClose( &jsonBuffer );
+        errcodesMap[5].errCode = json_objOpen( &jsonBuffer, NULL );
+        errcodesMap[6].errCode = json_objClose( &jsonBuffer );
+        errcodesMap[7].errCode = json_arrClose( &jsonBuffer );
+        errcodesMap[8].errCode = json_objClose( &jsonBuffer );
+        errcodesMap[9].errCode = json_end( &jsonBuffer );
+        printErrCodeMap(errcodesMap, sizeof(errcodesMap)/sizeof(errcodesMap[0]));
+        printf( "\n\n%s\n\n", buff );
+
         static char const rslt[] = "{\"a\":[{},{}]}";
-        check( p - buff == sizeof rslt - 1 );
+        check(strlen(buff) == sizeof rslt - 1 );
         check( 0 == strcmp( buff, rslt ) );
     }
     done();
@@ -135,14 +223,34 @@ static int empty( void ) {
 
 static int primitive( void ) {
     char buff[512];
-    char* p = json_objOpen( buff, NULL );
-    p = json_verylong( p, "max",  LONG_LONG_MAX );
-    p = json_verylong( p, "min",  LONG_LONG_MIN );
-    p = json_bool( p, "boolvar0", 0 );
-    p = json_bool( p, "boolvar1", 1 );
-    p = json_null( p, "nullvar" );
-    p = json_objClose( p );
-    p = json_end( p );
+    json_buffer_t jsonBuffer = {
+            .buffer = buff,
+            .total_sz = sizeof(buff),
+            .remaining_sz = sizeof(buff)
+    };
+    json_errcodes_map_t errcodesMap[] = {
+            {.funcName = "json_start"},
+            {.funcName = "json_objOpen"},
+            {.funcName = "json_verylong"},
+            {.funcName = "json_verylong"},
+            {.funcName = "json_bool"},
+            {.funcName = "json_bool"},
+            {.funcName = "json_null"},
+            {.funcName = "json_objClose"},
+            {.funcName = "json_end"}
+    };
+    errcodesMap[0].errCode = json_start(&jsonBuffer);
+    errcodesMap[1].errCode = json_objOpen( &jsonBuffer, NULL );
+    errcodesMap[2].errCode = json_verylong( &jsonBuffer, "max", LONG_LONG_MAX);
+    errcodesMap[3].errCode = json_verylong( &jsonBuffer, "min", LONG_LONG_MIN);
+    errcodesMap[4].errCode = json_bool( &jsonBuffer, "boolvar0", false );
+    errcodesMap[5].errCode = json_bool( &jsonBuffer, "boolvar1", true);
+    errcodesMap[6].errCode = json_null( &jsonBuffer, "nullvar");
+    errcodesMap[7].errCode = json_objClose( &jsonBuffer );
+    errcodesMap[8].errCode = json_end( &jsonBuffer );
+    printErrCodeMap(errcodesMap, sizeof(errcodesMap)/sizeof(errcodesMap[0]));
+    printf( "\n\n%s\n\n", buff );
+
     static char const rslt[] =  "{"
                                     "\"max\":9223372036854775807,"
                                     "\"min\":-9223372036854775808,"
@@ -150,89 +258,161 @@ static int primitive( void ) {
                                     "\"boolvar1\":true,"
                                     "\"nullvar\":null"
                                 "}";
-    check( p - buff == sizeof rslt - 1 );
+    check(strlen(buff) == sizeof rslt - 1 );
     check( 0 == strcmp( buff, rslt ) );
     done();
 }
 
+
 static int integers( void ) {
+    char buff[64];
+    json_buffer_t jsonBuffer = {
+            .buffer = buff,
+            .total_sz = sizeof(buff),
+            .remaining_sz = sizeof(buff)
+    };
     {
-        char buff[64];
-        char* p = json_objOpen( buff, NULL );
-        p = json_int( p, "a", 0 );
-        p = json_int( p, "b", 1 );
-        p = json_objClose( p );
-        p = json_end( p );
+        json_errcodes_map_t errcodesMap[] = {
+                {.funcName = "json_start"},
+                {.funcName = "json_objOpen"},
+                {.funcName = "json_int"},
+                {.funcName = "json_int"},
+                {.funcName = "json_objClose"},
+                {.funcName = "json_end"}
+        };
+        errcodesMap[0].errCode = json_start(&jsonBuffer);
+        errcodesMap[1].errCode = json_objOpen( &jsonBuffer, NULL );
+        errcodesMap[2].errCode = json_int( &jsonBuffer, "a", 0 );
+        errcodesMap[3].errCode = json_int( &jsonBuffer, "b", 1 );
+        errcodesMap[4].errCode = json_objClose( &jsonBuffer );
+        errcodesMap[5].errCode = json_end( &jsonBuffer );
+        printErrCodeMap(errcodesMap, sizeof(errcodesMap)/sizeof(errcodesMap[0]));
+        printf( "\n\n%s\n\n", buff );
+
         static char const rslt[] = "{\"a\":0,\"b\":1}";
-        check( p - buff == sizeof rslt - 1 );
+        check( strlen(buff) == sizeof rslt - 1 );
         check( 0 == strcmp( buff, rslt ) );
     }
     {
-        char buff[64];
-        char* p = json_objOpen( buff, NULL );
-        p = json_int( p, "max", INT_MAX );
-        p = json_int( p, "min", INT_MIN );
-        p = json_objClose( p );
-        p = json_end( p );
+        json_errcodes_map_t errcodesMap[] = {
+                {.funcName = "json_start"},
+                {.funcName = "json_objOpen"},
+                {.funcName = "json_int"},
+                {.funcName = "json_int"},
+                {.funcName = "json_objClose"},
+                {.funcName = "json_end"}
+        };
+        errcodesMap[0].errCode = json_start(&jsonBuffer);
+        errcodesMap[1].errCode = json_objOpen( &jsonBuffer, NULL );
+        errcodesMap[2].errCode = json_int( &jsonBuffer, "max", INT_MAX );
+        errcodesMap[3].errCode = json_int( &jsonBuffer, "min", INT_MIN );
+        errcodesMap[4].errCode = json_objClose( &jsonBuffer );
+        errcodesMap[5].errCode = json_end( &jsonBuffer );
+        printErrCodeMap(errcodesMap, sizeof(errcodesMap)/sizeof(errcodesMap[0]));
+        printf( "\n\n%s\n\n", buff );
+
         char rslt[ sizeof buff ];
         int len = sprintf( rslt, "{\"max\":%d,\"min\":%d}", INT_MAX, INT_MIN );
         check( len < sizeof buff );
-        check( p - buff == len );
+        check(strlen(buff) == len );
         check( 0 == strcmp( buff, rslt ) );
     }
     {
-        char buff[64];
-        char* p = json_objOpen( buff, NULL );
-        p = json_uint( p, "max", UINT_MAX );
-        p = json_objClose( p );
-        p = json_end( p );
+        json_errcodes_map_t errcodesMap[] = {
+                {.funcName = "json_start"},
+                {.funcName = "json_objOpen"},
+                {.funcName = "json_uint"},
+                {.funcName = "json_objClose"},
+                {.funcName = "json_end"}
+        };
+        errcodesMap[0].errCode = json_start(&jsonBuffer);
+        errcodesMap[1].errCode = json_objOpen( &jsonBuffer, NULL );
+        errcodesMap[2].errCode = json_uint( &jsonBuffer, "max", UINT_MAX );
+        errcodesMap[3].errCode = json_objClose( &jsonBuffer );
+        errcodesMap[4].errCode = json_end( &jsonBuffer );
+        printErrCodeMap(errcodesMap, sizeof(errcodesMap)/sizeof(errcodesMap[0]));
+        printf( "\n\n%s\n\n", buff );
+
         char rslt[ sizeof buff ];
         int len = sprintf( rslt, "{\"max\":%u}", UINT_MAX );
         check( len < sizeof buff );
-        check( p - buff == len );
+        check(strlen(buff) == len );
         check( 0 == strcmp( buff, rslt ) );
     }
     {
-        char buff[64];
-        char* p = json_objOpen( buff, NULL );
-        p = json_long( p, "max", LONG_MAX );
-        p = json_long( p, "min", LONG_MIN );
-        p = json_objClose( p );
-        p = json_end( p );
+        json_errcodes_map_t errcodesMap[] = {
+                {.funcName = "json_start"},
+                {.funcName = "json_objOpen"},
+                {.funcName = "json_long"},
+                {.funcName = "json_long"},
+                {.funcName = "json_objClose"},
+                {.funcName = "json_end"}
+        };
+        errcodesMap[0].errCode = json_start(&jsonBuffer);
+        errcodesMap[1].errCode = json_objOpen( &jsonBuffer, NULL );
+        errcodesMap[2].errCode = json_long( &jsonBuffer, "max", LONG_MAX);
+        errcodesMap[3].errCode = json_long( &jsonBuffer, "min", LONG_MIN);
+        errcodesMap[4].errCode = json_objClose( &jsonBuffer );
+        errcodesMap[5].errCode = json_end( &jsonBuffer );
+        printErrCodeMap(errcodesMap, sizeof(errcodesMap)/sizeof(errcodesMap[0]));
+        printf( "\n\n%s\n\n", buff );
+
         char rslt[ sizeof buff ];
         int len = sprintf( rslt, "{\"max\":%ld,\"min\":%ld}", LONG_MAX, LONG_MIN );
         check( len < sizeof buff );
-        check( p - buff == len );
+        check( strlen(buff) == len );
         check( 0 == strcmp( buff, rslt ) );
     }
     {
-        char buff[64];
-        char* p = json_objOpen( buff, NULL );
-        p = json_ulong( p, "max", ULONG_MAX );
-        p = json_objClose( p );
-        p = json_end( p );
+        json_errcodes_map_t errcodesMap[] = {
+                {.funcName = "json_start"},
+                {.funcName = "json_objOpen"},
+                {.funcName = "json_ulong"},
+                {.funcName = "json_objClose"},
+                {.funcName = "json_end"}
+        };
+        errcodesMap[0].errCode = json_start(&jsonBuffer);
+        errcodesMap[1].errCode = json_objOpen( &jsonBuffer, NULL );
+        errcodesMap[2].errCode = json_ulong( &jsonBuffer, "max", ULONG_MAX);
+        errcodesMap[3].errCode = json_objClose( &jsonBuffer );
+        errcodesMap[4].errCode = json_end( &jsonBuffer );
+        printErrCodeMap(errcodesMap, sizeof(errcodesMap)/sizeof(errcodesMap[0]));
+        printf( "\n\n%s\n\n", buff );
+
         char rslt[ sizeof buff ];
         int len = sprintf( rslt, "{\"max\":%lu}", ULONG_MAX );
         check( len < sizeof buff );
-        check( p - buff == len );
+        check( strlen(buff) == len );
         check( 0 == strcmp( buff, rslt ) );
     }
     {
-        char buff[64];
-        char* p = json_objOpen( buff, NULL );
-        p = json_verylong( p, "max", LONG_LONG_MAX );
-        p = json_verylong( p, "min", LONG_LONG_MIN );
-        p = json_objClose( p );
-        p = json_end( p );
+        json_errcodes_map_t errcodesMap[] = {
+                {.funcName = "json_start"},
+                {.funcName = "json_objOpen"},
+                {.funcName = "json_verylong"},
+                {.funcName = "json_verylong"},
+                {.funcName = "json_objClose"},
+                {.funcName = "json_end"}
+        };
+        errcodesMap[0].errCode = json_start(&jsonBuffer);
+        errcodesMap[1].errCode = json_objOpen( &jsonBuffer, NULL );
+        errcodesMap[2].errCode = json_verylong( &jsonBuffer, "max", LONG_LONG_MAX);
+        errcodesMap[3].errCode = json_verylong( &jsonBuffer, "min", LONG_LONG_MIN);
+        errcodesMap[4].errCode = json_objClose( &jsonBuffer );
+        errcodesMap[5].errCode = json_end( &jsonBuffer );
+        printErrCodeMap(errcodesMap, sizeof(errcodesMap)/sizeof(errcodesMap[0]));
+        printf( "\n\n%s\n\n", buff );
+
         char rslt[ sizeof buff ];
         int len = sprintf( rslt, "{\"max\":%lld,\"min\":%lld}", LONG_LONG_MAX, LONG_LONG_MIN );
         check( len < sizeof buff );
-        check( p - buff == len );
+        check( strlen(buff) == len );
         check( 0 == strcmp( buff, rslt ) );
     }
     done();
 }
 
+#if 0
 static int array( void ) {
     char buff[64];
     char* p = json_objOpen( buff, NULL );
@@ -270,6 +450,7 @@ static int real( void ) {
     done();
 }
 
+#endif
 // --------------------------------------------------------- Execute tests: ---
 
 int main( void ) {
@@ -279,8 +460,10 @@ int main( void ) {
         { empty,     "Empty objects and arrays" },
         { primitive, "Primitives values"        },
         { integers,  "Integers values"          },
+#if 0
         { array,     "Array"                    },
         { real,      "Real"                     }
+#endif
     };
     return test_suit( tests, sizeof tests / sizeof *tests );
 }
